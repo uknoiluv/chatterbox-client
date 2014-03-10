e98778b91dc398a65ce913672d4402e618abcf25
Josh <jsgoldberg90@gmail.com>
2014-03-10 14:06:56 -0700

    basic setup

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,47 @@
 // YOUR CODE HERE:
 
+var retrieve = function(){
+	$.ajax({
+		url: 'https://api.parse.com/1/classes/chatterbox',
+		type: 'GET',
+		data: JSON.stringify(message),
+		contentType: 'application/json',
+		success: function (data) {
+			for (var i = 0; i < data.results.length; i ++){
+				chatMessage = data.results[i].text;
+				$('#main').append(chatMessage);
+			}
+		},
+		error: function (data) {
+			// see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+			console.error('chatterbox: Failed to send message');
+		}
+	});
+};
+
+var send = function(message) {
+	$.ajax({
+		// always use this url
+		url: 'https://api.parse.com/1/classes/chatterbox',
+		type: 'POST',
+		data: JSON.stringify(message),
+		contentType: 'application/json',
+		success: function (data) {
+			console.log('chatterbox: Message sent');
+		},
+		error: function (data) {
+		// see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+			console.error('chatterbox: Failed to send message');
+		}
+	});
+};
+
+var message = {
+  'username': 'shawndrost',
+  'text': 'trololo',
+  'roomname': '4chan'
+};
+
+send(message);
+var chatMessages = retrieve();
+
