7075ca4cb0ed0d67069d9566a7c31868119d41f1
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-10 16:34:15 -0700

    incomeplete rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,56 @@
 // YOUR CODE HERE:
+/* global $ */
 
-var retrieve = function(){
+var message = {
+  'username': 'TROLL',
+  'text': 'hello',
+  'roomname': 'Troll Room'
+};
+
+var messageTemplate = function(result){
+	var userName = result.username.charAt(0).toUpperCase() + result.username.slice(1);
+	var resultText = result.text.charAt(0).toUpperCase() + result.text.slice(1);
+	var resultRoom = result.roomname.charAt(0).toUpperCase() + result.roomname.slice(1);
+	var message = $('<span>' + userName +': ' + resultText + ' - ' + resultRoom +  '</span><br/><br/>');
+	return message;
+};
+
+var cleanData = function(user, text, room){
+	var verdict = true;
+	var symbols = ['<','>','-','!','{', '%', '"']
+	if (user === undefined || symbols.indexOf(user.slice(0,1)) !== -1){
+    verdict = false;
+	}
+	if (text === undefined || symbols.indexOf(text.slice(0,1)) !== -1 ){
+    verdict = false;
+	}
+  if (room === undefined){
+  	verdict = true;
+	} else {
+		verdict =  symbols.indexOf(room.slice(0,1)) !== -1 ? false : true;
+	}
+	return verdict;
+};
+
+var retrieve = function(room){
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'GET',
-		data: JSON.stringify(message),
+		data: {'order':'-createdAt'},
 		contentType: 'application/json',
 		success: function (data) {
-			for (var i = 0; i < data.results.length; i ++){
-				chatMessage = data.results[i].text;
-				$('#main').append(chatMessage);
+			var rooms = {};
+			for (var i = 0; i < data.results.length; i++){
+				var result = data.results[i];
+				rooms[result.roomname] = result.roomname;
+				if(cleanData(result.username, result.text, result.roomname) === true){
+					var message = messageTemplate(result);
+					$('#main').append(message);
+				}
+			}
+			for (var key in rooms){
+				var room = $('<option value="'+key+'">'+key+'</option>');
+				$('#rooms').append(room);
 			}
 		},
 		error: function (data) {
@@ -36,12 +77,8 @@ var send = function(message) {
 	});
 };
 
-var message = {
-  'username': 'shawndrost',
-  'text': 'trololo',
-  'roomname': '4chan'
-};
+
 
 send(message);
-var chatMessages = retrieve();
 
+retrieve();
