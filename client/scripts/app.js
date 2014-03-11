dcb17d9dd04622836a11c202055feaec493e1e29
Hack Reactor Students <uknoiluvu@gmail.com>
2014-03-10 21:11:58 -0700

    complete basic requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,10 @@
 // YOUR CODE HERE:
 /* global $, _ */
 
+var user = window.location.search.slice(10);
+var friends = {};
+console.log(user);
+
 var message = {
   'username': 'Chef',
   'text': 'hello Children',
@@ -9,7 +13,10 @@ var message = {
 
 // message template
 var messageTemplate = function(user, text, room){
-	var message = $('<span>' + user +': ' + text + ' - ' + room +  '</span><br/><br/>');
+	var userSpan = '<a href="#" class="'+user+'">'+user+'</a>';
+	var textSpan = '<span class="'+text+'">'+text+'</span>';
+	var roomSpan = '<span class="'+room+'">'+room+'</span>';
+	var message = $('<p>' + userSpan +': ' + textSpan + ' - ' + roomSpan +  '</p>');
 	return message;
 };
 
@@ -22,28 +29,60 @@ var sanitize = function(string){
 var listRooms = function(rooms){
 	for (var key in rooms){
 		$('#rooms').append('<option value="'+key+'">'+key+'</option>');
+		$('#selectRoom').append('<option value="'+key+'">'+key+'</option>');
 	}
 };
+
+// store friends
+var storeFriends = function(friend){
+	friends[friend] = friend;
+};
+
+var makeFriendsBold = function(){
+  $.each($('p'), function(){
+		var friend = $(this).find('a').html();
+		if (friends[friend]) {
+			$(this).children().css({
+				'font-weight': 'bold'
+			});
+		}
+  });
+};
+
+// filter by room
+var filterRoom = function(roomName){
+	var spanArray = $('p');
+	$.each(spanArray, function(){
+		if (!$(this).find('span').hasClass(roomName)) {
+			$(this).fadeOut('fast');
+		} else {
+			$(this).fadeIn();
+		}
+	});
+};
+
 
 // function to call on successful retreival of data
 var retrieveOnSuccess = function(resultData){
 	var rooms = {};
+	resultData = _.sortBy(resultData, 'createdAt');
 	_.each(resultData, function(item) {
-		var room = $('<span class="userSpan">'+item.roomname+'</span>').text();
+		var room = $('<span>'+item.roomname+'</span>').text();
 		room = sanitize(room);
 		rooms[room] = room;
-		var text = $('<span class="textSpan">'+item.text+'</span>').text();
-		var user = $('<span class="textSpan">'+item.username+'</span>').text();
-		console.log(room, text, user);
+		var text = $('<span>'+item.text+'</span>').text();
+		text = sanitize(text);
+		var user = $('<span>'+item.username+'</span>').text();
+		user = sanitize(user);
 		var message = messageTemplate(user, text, room);
-		$('#main').append(message);
+		$('#messagesDiv').prepend(message);
 	});
 	listRooms(rooms);
 };
 
 
 // retrieve messages
-var retrieve = function(specificRoom){
+var retrieve = function(){
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'GET',
@@ -51,12 +90,19 @@ var retrieve = function(specificRoom){
 		contentType: 'application/json',
 		success: function(data){
 			retrieveOnSuccess(data.results);
+			if ($('#rooms').val() !== 'All Rooms') {
+				filterRoom($('#rooms').val());
+			} else {
+				$('p').show();
+			}
 		},
 		error: function (data) {
 			console.error('chatterbox: Failed to send message');
 		}
 	});
 };
+
+
 
 // send messages
 var send = function(message) {
@@ -74,25 +120,46 @@ var send = function(message) {
 	});
 };
 
+
+
 // document ready functions/events
 $(document).ready(function(){
 	$('#refresh').on('click',function(){
-		console.log('hello');
 		retrieve();
-	});
+  });
 
-	$('#rooms').on('change', function(item){
-		var room = $(this).val();
-		// console.log(room);
-		$('span, br').remove();
+  $('#sendMsg').on('click',function(){
+		var msg = {
+			'username': user,
+			'text': $('#textMsg').val(),
+			'roomname': $('#selectRoom').val()
+		};
+    send(msg);
+  });
 
+  $('#friendsFilter').on('click',function(){
+		$.each($('p'), function(){
+			var friend = $(this).find('a').html();
+			if (!friends[friend]) {
+				$(this).hide();
+			}
+		});
+  });
+
+  $('body').on('click', 'a', function(e){
+		e.preventDefault();
+		storeFriends($(this).html());
+		makeFriendsBold();
+  });
+
+
+	$('#rooms').on('change', function(){
+		var roomName = $(this).val();
+		roomName === 'All Rooms' ? $('p').show() : filterRoom(roomName);
 	});
 });
 
 
 
 
-
-
-send(message);
 retrieve();
